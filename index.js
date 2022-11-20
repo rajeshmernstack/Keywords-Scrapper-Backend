2af065434c54d42b17a50ae1fbe0c6a8cba49360
Rajesh Kumar <rajesh.bcsf18@iba-suk.edu.pk>
2022-11-20 13:46:46 +0500

    updated

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.get('/keyword/:seed', async (req, res) => {
 
     var data = [];
     const browser = await puppeteer.launch({
-        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
         timeout: 60000,
     });
