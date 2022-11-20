8b4339a84a423aff331770a166d4640347b46805
Rajesh Kumar <rajesh.bcsf18@iba-suk.edu.pk>
2022-11-20 12:14:29 +0500

    fixed bugs

--- index.js
+++ index.js
@@ -7,30 +7,28 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome" })
 })
 
-app.get('/keyword/:seed', (req, res) => {
+app.get('/keyword/:seed', async (req, res) => {
     let keyword = req.params.seed;
 
-    (async () => {
-        var data = [];
-        const browser = await puppeteer.launch({
-            headless: false,
-            args: ['--no-sandbox'],
-            timeout: 60000,
-        });
-        const page = await browser.newPage();
-        await page.setCacheEnabled(true);
-        await page.goto(`https://www.keyword.io/tool/google-longtail-finder?q=${keyword}&audience=en-us`);
-        data = await page.$$eval('table tr td', tds => tds.map((td) => {
-            return td.innerText;
-        }));
-        data = data.filter(n => n)
+    var data = [];
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox'],
+        timeout: 60000,
+    });
+    const page = await browser.newPage();
+    await page.setCacheEnabled(true);
+    await page.goto(`https://www.keyword.io/tool/google-longtail-finder?q=${keyword}&audience=en-us`);
+    data = await page.$$eval('table tr td', tds => tds.map((td) => {
+        return td.innerText;
+    }));
 
-        res.json({ data: data });
+    await browser.close();
+    data = data.filter(n => n)
+    res.json({ data: data });
 
 
-        await browser.close();
-    })();
-    res.json({status: "The app takes too much time"})
+
 })
 app.get("/status", (req, res) => {
     res.json({ status: 1, message: "Server is Running" })
